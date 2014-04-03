f8d6bb534e3b07911103f115f852deafe676d733
Fidel <332700@gmail.com>
2014-04-03 23:41:05 +0800

    modify TestList

--- src/pratice/lesson7/TestList.java
+++ src/pratice/lesson7/TestList.java
@@ -14,6 +14,8 @@ public class TestList {
 		books.add(new String("JAVA EE轻量级实践"));
 		books.add(new String("Structs 权威指南"));
 		books.add(new String("基于AJAX的权威应用宝典"));
+		books.add(new String("JAVA EE轻量级实践"));
+
 		System.out.println(books);
 		books.add(1,new String("ROR 敏捷开发最佳实践"));
 		System.out.println(books);
@@ -22,9 +24,16 @@ public class TestList {
 		}
 		books.remove(2);
 		System.out.println(books);
-		System.out.println(books.indexOf(new String("基于AJAX的权威应用宝典")));
+		System.out.println(books.indexOf(new String("JAVA EE轻量级实践")));
+		//通过调用eauals方法查找对象，并删除第一个匹配的对象
+		System.out.println(books);
+		books.remove(new String("JAVA EE轻量级实践"));
+		System.out.println(books);
+
 		books.set(1, new String("Structs 权威指南"));
 		System.out.println(books);
+		System.out.println(books.subList(1,3));
+
 
 	}
 
